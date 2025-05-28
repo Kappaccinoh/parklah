98c25e424b6213b0f178e6a30fd4a57931d6ed42
Kappaccinoh <lim.jia28@gmail.com>
2025-05-28 23:49:02 +0800

    publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
+const repo = 'parklah';
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // produce a pure static export
+  output: 'export',
+
+  // when serving from GitHub Pages
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
